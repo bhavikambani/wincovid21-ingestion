a685236276d5e8b65983797e381ad0b4a040c844
Bhavik Ambani <bhavikkumar.ambani@myntra.com>
2021-04-30 16:23:32 +0530

    Initial Commit

---
+++ src/main/java/com/covimyn/ingestion/CoviMynIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.covimyn.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoviMynIngestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoviMynIngestionApplication.class, args);
+    }
+
+}
