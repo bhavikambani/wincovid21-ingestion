dec44cfb63a52dd91bc2996655785ee6ca34eec4
Bhavik Ambani <bhavikkumar.ambani@myntra.com>
2021-05-01 15:38:25 +0530

    First commit

--- target/classes/static/schema.sql
+++
@@ -1,7 +0,0 @@
-create table user_actions_audit (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    resource_id int NOT NULL,
-    feedback_type varchar(512) NOT NULL,
-	updated_on DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
