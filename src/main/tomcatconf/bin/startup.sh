e6df38b2adedac88b2d9cc8997298a34a6c4c8ac
Bhavik Ambani <bhavikkumar.ambani@myntra.com>
2021-05-14 14:19:04 +0530

    Startups cript change

--- src/main/tomcatconf/bin/startup.sh
+++ src/main/tomcatconf/bin/startup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 java -XX:-OmitStackTraceInFastThrow -server  -XX:-OmitStackTraceInFastThrow -XX:+UseStringDeduplication -XX:+DisableExplicitGC -XX:+UnlockExperimentalVMOptions -Dlogging.path=/data/wincovid21-ingestion/wincovid21-ingestion/  -Xms16384m -XX:+UseG1GC -Xmx16384m -jar -Dspring.profiles.active=prod  /data/wincovid21-ingestion/wincovid21-ingestion/target/wincovid21-ingestion.jar  &> /data/wincovid21-ingestion/wincovid21-ingestion/console.log & disown
+
+### In case want to use JDK16
+### /data/tools/jdk16/jdk-16.0.1/bin/java -XX:-OmitStackTraceInFastThrow -XX:+UnlockExperimentalVMOptions -XX:+UseStringDeduplication -XX:+UseZGC -Xlog:gc -XX:+UnlockDiagnosticVMOptions -XX:-ZProactive -Xlog:safepoint,classhisto*=trace,age*,gc*=info:file=/data/wincovid21-ingestion/wincovid21-ingestion/gclogs/gc-%t.log:time,tid,tags:filecount=50,filesize=50m -Xms16384m -Xmx16384m -jar -Dspring.profiles.active=prod  /data/wincovid21-ingestion/wincovid21-ingestion/target/wincovid21-ingestion.jar  &> /data/wincovid21-ingestion/wincovid21-ingestion/console.log & disown
